a2a6221ec5054804eeb6c56064810043102de7d9
Sprite_Spirit <Sprite.spirit.t.me@gmail.com>
2024-06-06 22:47:16 +0300

    Add pages and info

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import index
-
+from main.views import index, clients, dashboard, about
 
 app_name = MainConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
+    path('clients/', clients, name='clients'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('about/', about, name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def clients(request):
+    return render(request, 'main/clients.html')
+
+
+def dashboard(request):
+    return render(request, 'main/dashboard.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
